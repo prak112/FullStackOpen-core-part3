069705f6f9023159c6178ce749d20844a434050e
PrakGov <55034062+prak112@users.noreply.github.com>
2024-05-02 20:00:08 +0300

    ex 3.1 to 3.6 completed

---
+++ index.js
@@ -0,0 +1,102 @@
+const express = require('express')
+const app = express()
+const PORT = 3001
+app.use(express.json()) // json-parser for POST request
+
+// data
+let contacts = 
+[
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+// HTTP methods to access data
+//Root
+app.get('/', (request, response) => {
+    response.send('<h1>Welcome to Phonebook app</h1>');
+})
+
+// GET
+app.get('/api/contacts', (request, response) => {
+    response.json(contacts)
+})
+
+app.get('/api/info', (request, response) => {
+    const content = `Phonebook has information for ${contacts.length} contacts`
+    const now = new Date()
+    const lastRequestedAt = now.toString()
+    response.send(`<h2>${content}</h2></br><p>${lastRequestedAt}</p>`);
+})
+
+app.get('/api/contacts/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const contact = contacts.find((contact) => contact.id === id)
+    if(!contact){
+        response.status(404).send('Contact does not exist. Verify contact ID.');
+    }
+    else{ response.json(contact) }
+})
+
+// POST
+app.post('/api/contacts', (request, response) => {
+    console.log('Before POST:\n', contacts)
+
+    const generateId = (limit) => Math.floor(Math.random() * limit)
+    const name = request.body.name
+    const number = request.body.number
+    if(!name || !number){
+        return response.status(400).json({
+            ERROR: "Missing Contact information. Verify Name and/or Number."
+        })
+    }
+    else if(contacts.some(contact => contact.name === name)){
+        return response.status(400).json({
+            ERROR: "Contact Name already exists."
+        })
+    }
+
+    const contact = {
+        id: generateId(500),
+        name: name,
+        number: number  
+    }
+    contacts.concat(contact)
+    console.log('New Contact: ', contact);
+    console.log('After POST:\n', contacts.concat(contact))
+    response.json(contacts.concat(contact))
+})
+
+// DELETE
+app.delete('/api/contacts/:id', (request, response) => {
+    const id = Number(request.params.id)
+    contacts = contacts.filter((contact) => contact.id !== id)
+    console.log(`Contact of ID ${id} deleted.`);
+    console.log(contacts)
+    response.status(204).json({
+        INFO: "Contact deleted"
+    })
+})
+
+
+// listen to PORT and log
+app.listen(PORT, () => {
+    console.log(`Server running on Port ${PORT}`)
+})
